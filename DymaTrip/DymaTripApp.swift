ef72b7d42e25a1a4827d3c93edfd2571faa60a9c
codewitholivier as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-10-16 16:38:57 +0200

    Initial Commit

---
+++ DymaTrip/DymaTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  DymaTripApp.swift
+//  DymaTrip
+//
+//  Created by olivier geiger on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct DymaTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
